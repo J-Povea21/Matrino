d432d575f1f048b41fc8e7afef4e71e5cab57b4b
Amdres <japovea@uninorte.edu.co>
2024-06-15 18:26:32 -0500

    Sage on the road! All the functionalities are now powered with the SageMath engine

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from src.api.v1.endpoints import router as v1_router
+from src.api.v1 import router as v1_router
 
 app = FastAPI()
 
@@ -12,8 +13,24 @@ app.add_middleware(
 )
 
 # Routers for versions
-app.include_router(v1_router, prefix='/v1')
+app.include_router(v1_router.router, prefix='/v1')
 
 @app.get('/')
 def read_root():
     return {'message': 'Welcome to Matrino :)'}
+
+# Exceptions handlers
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={'success': False, 'message': exc.detail}
+    )
+
+@app.exception_handler(Exception)
+async def exception_handler(request, exc):
+    return JSONResponse(
+        status_code=500,
+        content={'success': False, 'message': 'An unknown error happened. Try later'}
+    )
--- src/schemas/v1/schemas.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class Operation(BaseModel):
-    a: float
-    b: float
